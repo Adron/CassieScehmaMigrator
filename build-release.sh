00d07d58fc20493bfad064890d6b603bf0fe69e0
Adron Hall <adronhall@gmail.com>
2019-11-06 15:41:14 -0800

    The release is obviously to big... for a command that prints out literally one @#$%&^#$%^ thing this is insane.

--- build-release.sh
+++ build-release.sh
@@ -7,7 +7,7 @@ function publish {
     echo $1
     CASSIEPATH=$1
     RID=$1
-    CASSIEPATH=release/$CASSIEPATH
+    CASSIEPATH=release/v0/$CASSIEPATH
     
     dotnet publish ./CassieConsole/CassieConsole.csproj -c Release -r $RID -o $CASSIEPATH  /p:PublishSingleFile=true
     mv ./$CASSIEPATH/CassieConsole ./$CASSIEPATH/casma  
@@ -22,7 +22,7 @@ publish "osx.10.13-x64"
 
 # The outlier odd build for the *.exe file for Windows.
 RID="win-x64"
-CASSIEPATH=release/$RID
+CASSIEPATH=release//v0/$RID
 dotnet publish ./CassieConsole/CassieConsole.csproj -c Release -r  -o $CASSIEPATH  /p:PublishSingleFile=true
 mv ./$CASSIEPATH/CassieConsole.exe ./$CASSIEPATH/casma.exe  
 rm ./$CASSIEPATH/CassieConsole.pdb
